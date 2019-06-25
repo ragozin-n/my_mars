64646263d55f6b4a07c4b8c2a5447f61799bfa31
Ragozin Nikita <ragozin-n@protonmail.com>
2019-06-25 14:59:37 +0500

    Add react-native-gesture-handler + react-native-sensitive-info to project (also link it)

--- android/app/src/main/java/com/my_mars/MainApplication.java
+++ android/app/src/main/java/com/my_mars/MainApplication.java
@@ -3,6 +3,8 @@ package com.my_mars;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
+import br.com.classapp.RNSensitiveInfo.RNSensitiveInfoPackage;
 import com.reactnativecommunity.asyncstorage.AsyncStoragePackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -24,6 +26,8 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNGestureHandlerPackage(),
+            new RNSensitiveInfoPackage(),
             new AsyncStoragePackage()
       );
     }
